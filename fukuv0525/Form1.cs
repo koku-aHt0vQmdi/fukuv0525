4a032e9c078a9c6140b55009b294ffc0ea5e5638
田渕 <tabuti.dat@gmail.com>
2023-05-25 13:36:05 +0900

    復習問題

--- fukuv0525/Form1.cs
+++ fukuv0525/Form1.cs
@@ -6,5 +6,31 @@ namespace fukuv0525
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ìcü∫");
+            label1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top -= 15;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left += 15;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left -= 15;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top += 15;
+        }
     }
 }
